5d1c4fbd7b0e526e62ab794a7b473d982c83e518
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-04-07 11:05:08 -0400

    feat: init project.

---
+++ lib/github_search.ex
@@ -0,0 +1,18 @@
+defmodule GithubSearch do
+  @moduledoc """
+  Documentation for GithubSearch.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GithubSearch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/github_search_test.exs
@@ -0,0 +1,8 @@
+defmodule GithubSearchTest do
+  use ExUnit.Case
+  doctest GithubSearch
+
+  test "greets the world" do
+    assert GithubSearch.hello() == :world
+  end
+end
